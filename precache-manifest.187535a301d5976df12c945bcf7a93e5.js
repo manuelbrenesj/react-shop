483d16ee17106d71e77aa55fb23d32b90d9f893c
unknown <manuelbrenesj@gmail.com>
2019-11-06 13:27:16 -0600

    Updates

---
+++ precache-manifest.187535a301d5976df12c945bcf7a93e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed209ee2936520836420290a457139c",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "95f235709f5425b85ee3",
+    "url": "/react-shop/static/css/main.436b32ad.chunk.css"
+  },
+  {
+    "revision": "1c06946f5d784794431a",
+    "url": "/react-shop/static/js/2.f591949d.chunk.js"
+  },
+  {
+    "revision": "95f235709f5425b85ee3",
+    "url": "/react-shop/static/js/main.19a069b1.chunk.js"
+  },
+  {
+    "revision": "f19f88b8df90dd91e960",
+    "url": "/react-shop/static/js/runtime-main.ec45d887.js"
+  },
+  {
+    "revision": "ec2e862a3296ceb480352f59840697d1",
+    "url": "/react-shop/static/media/hero.ec2e862a.jpg"
+  },
+  {
+    "revision": "310d8273fe320e6b535fb5af8b46aaea",
+    "url": "/react-shop/static/media/icon.310d8273.svg"
+  },
+  {
+    "revision": "630af4013de8bee3772278ebf075f334",
+    "url": "/react-shop/static/media/shopping-cart.630af401.svg"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "/react-shop/static/media/user.ed52f187.svg"
+  }
+]);
